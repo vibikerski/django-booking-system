561e072c6f63c5cfe073a48c7d149f1148184ec7
vibikerski <vibikerski@gmail.com>
2024-02-17 16:47:09 +0100

    Improved models

--- booking_system/admin.py
+++ booking_system/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from booking_system.models import User, Hotel, Room, Booking
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Hotel)
+admin.site.register(Room)
+admin.site.register(Booking)
