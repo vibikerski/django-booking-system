17c81267bbe3fd8be83797131738311ee431d728
vibikerski <vibikerski@gmail.com>
2024-02-23 18:22:23 +0100

    basic templates (not finished)

--- booking_system/models.py
+++ booking_system/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class User(models.Model):
     full_name = models.CharField(max_length=255)
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
     
     def __str__(self):
         return self.full_name
@@ -25,8 +25,8 @@ class Room(models.Model):
 class Booking(models.Model):
     room = models.ForeignKey(Room, on_delete=models.SET_NULL, null=True)
     booked_by = models.ForeignKey(User, on_delete=models.CASCADE)
-    start_time = models.DateField()
-    end_field = models.DateField()
+    start_date = models.DateField()
+    end_date = models.DateField()
     
     def __str__(self):
         return self.room.name
--- booking_system/views.py
+++ booking_system/views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, HttpResponse
+from booking_system.models import User, Hotel, Room, Booking
+from datetime import datetime
 
-# Create your views here.
+def check_available(bookings, start, end):
+    start = datetime(*start.split('-'))
+    end = datetime(*end.split('-'))
+    return not any(
+        start < booking.end_date or end > booking.start_date
+        for booking in bookings
+    )
+
+def book_a_room(request):
+    if request.method != "POST":
+        return HttpResponse('Not allowed', 405)
+    room_id = request.POST.get('room-id')
+    start_date = request.POST.get('start-date')
+    end_date = request.POST.get('end-date')
+
+    email = request.POST.get('email')
+    try:
+        user = User.objects.get(email=email)
+    except Exception:
+        username = request.POST.get('user-name')
+        user = User(
+            full_name=username,
+            email=email
+        )
+
+    room = get_object_or_404(Room, id=room_id)
+    if not check_available(Booking.objects.filter(room=room), start_date, end_date):
+        return HttpResponse('Not available', 400)
+
+    return Booking(
+        room=room, booked_by=user, start_date=start_date, end_date=end_date
+    )
+
+def get_booking_form(request):
+    pass # TODO: RENDER WITH TEMPLATES
+    # TODO: ONLY ALLOW SELECTING AVAILABLE DATES IN TEMPLATE (to justify httpresponse bad request)
+
+def get_hotels(request):
+    hotels = Hotel.objects.all()
+    context = {
+        'hotels': hotels
+    }
+    
+    return render(
+        request,
+        'booking_system/hotels.html',
+        context
+    )
+
+def get_hotel(request, hotel_id):
+    hotel = get_object_or_404(Hotel, id=hotel_id)
+    rooms = Room.objects.filter(hotel=hotel)
+    
+    context = {
+        'hotel': hotel,
+        'rooms': rooms
+    }
+    
+    return render(
+        request,
+        'booking_system/hotel.html',
+        context
+    )
